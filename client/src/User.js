e5e2dc7a1f37918329b3bd58a0b205cce597b47d
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2023-05-13 15:08:47 +0200

    Update User.js

--- client/src/User.js
+++ client/src/User.js
@@ -1,62 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
-
-
+import { FaWallet, FaTwitter } from 'react-icons/fa';
+import connectWallet from './walletconection';
 
 
 function User() {
-  return (
+  return ( 
     <div className="App">
-     
-
-     <div className="square-row__row">
-        <div className="color-square red-square span6" ng-class="{span4: !campaign.hide_total_entries, span6: campaign.hide_total_entries}">
-          <span className="square-describe" id="current-entries">
-            <span className="status ng-binding" ng-class="getContestantEntriesStatusClass()">
-              0
-            </span>
-            <span className="description ng-binding">Your Entries</span>
-          </span>
-        </div>
-        {/* ngIf: !campaign.hide_total_entries */}
-        <div className="color-square purple-square span6" ng-class="{span4: !campaign.hide_total_entries, span6: campaign.hide_total_entries}">
-          <div className="square-divider" />
-          <span className="square-describe ng-scope" data-ends={1684677612} data-starts={1642942800} gl-countdown ng-class="{'one-line': isEnded()}">
-            <span className="status ng-binding">
-              9
-            </span>
-            <span className="description ng-binding" ng-show="isStarted() || isTestMode()">
-              Days
-              {/* ngIf: !isEnded() && isStarted() */}<span ng-if="!isEnded() && isStarted()" className="ng-binding ng-scope">
-                Left
-              </span>{/* end ngIf: !isEnded() && isStarted() */}
-              {/* ngIf: !isEnded() && !isStarted() */}
-            </span>
-            {/* ngIf: !isStarted() && !isTestMode() */}
-          </span>
-        </div>
+      <div className="border col-5 container shadow" style={{'borderRadius':'5px 5px 0 0'}}>
+        <h2><b>9</b> days left</h2>
       </div>
 
-     <img ng-if="!show_image_above_name() && has_single_image()" className="crisp incentive-banner ng-scope top" ng-src="https://user-assets.out.sh/user-assets/579570/FEjhzxuZTPg0wo4G/where-to.gif" ng-style="{height: imageHeight()}" alt="NFT Giveaway" ng-class="{top: incentive.description}" applyonload src="https://user-assets.out.sh/user-assets/579570/FEjhzxuZTPg0wo4G/where-to.gif" style={{height: '283px'}} />
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<div>
-
-{/* <FontAwesomeIcon icon="fa-brands fa-square-twitter" /> */}
-<i className="fas fa-wallet" ></i>
-
-</div>
-
-
-     
+      <img ng-if="!show_image_above_name() && has_single_image()" className="crisp incentive-banner ng-scope top col-5" ng-src="https://user-assets.out.sh/user-assets/579570/FEjhzxuZTPg0wo4G/where-to.gif" ng-style="{height: imageHeight()}" alt="NFT Giveaway" ng-class="{top: incentive.description}" applyonload src="https://user-assets.out.sh/user-assets/579570/FEjhzxuZTPg0wo4G/where-to.gif"  />
+      
+      <div className="border col-5 container shadow pt-2" style={{'borderRadius':'0 0 5px 5px'}}>
+        <p> Promote your NFTs by giving some free ones away to users who 
+        complete meaningful actions that will build awareness and spread the word about your NFTs.</p>
+        <div style={{'display': 'flex'}} onClick={connectWallet}>
+          <div className="icon col-2">
+            <h4><FaWallet /></h4>
+          </div>
+          <div className="field">
+            <h4>Connect your Tezos Wallet</h4>
+          </div>
+        </div>
+        <div style={{'display': 'flex'}}>
+          <div className="icon col-2" style={{'background-color': '#00BFFF'}}>
+            <h4><FaTwitter /></h4>
+          </div>
+          <div className="field">
+            <h4>Sign in with Twitter account</h4>
+          </div>
+        </div>
+        <div className='footer'>
+          <p>Terms & Conditions | © NFT Creator</p>
+        </div>
+      </div>
     </div>
   );
 }
