6843d2ae6cb8e4895c1d8a8babf4fe989b82907f
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2023-05-13 15:09:05 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -22,9 +22,10 @@ import User from "./User";
  <BrowserRouter>
       <Routes>
 
-      <Route path="/" element={<Dashboard />}>
-          <Route index element={<Dashboard />} />
+      <Route path="/" element={<User />}>
+          <Route index element={<User />} />
           <Route path={"/user"} element={<User />} />
+     
           {/* <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} /> */}
         </Route>
